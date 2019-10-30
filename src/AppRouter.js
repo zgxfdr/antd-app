25199d3947f330ed5bea2688695fd140b6814067
zgxfdr <157384086@qq.com>
2019-10-30 15:38:54 +0800

    updated

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,10 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './styles/index.css';
 import TodoList from './component/TodoList'
-import Welcome from './component/Welcome'
-import Form from './component/FormBox'
-import Video from './component/Video'
-import Workplace from './component/Workplace'
 import { Menu, Icon } from 'antd';
 const { SubMenu } = Menu;
 
@@ -25,29 +21,15 @@ function AppRouter() {
                         title={
                             <span>
                                 <Icon type="appstore" />
-                                <span>一级导航</span>
+                                <span>TodoList</span>
                             </span>
                         }
                     >
-                         <Menu.Item key="1"><Link to="/Form">Form表单</Link> </Menu.Item>
-                        <Menu.Item key="2"><Link to="/welcome">欢迎页</Link> </Menu.Item>
-                        <Menu.Item key="3"><Link to="/">首页</Link> </Menu.Item>
-                        <SubMenu key="sub3" title="二级导航">
-                            <Menu.Item key="4"><Link to="/video/">视频教程</Link></Menu.Item>
-                            <Menu.Item key="5"><Link to="/workplace">职场技能</Link></Menu.Item>
-                        </SubMenu>
+                         <Menu.Item key="1"><Link to="/">Jehadsama</Link> </Menu.Item>
                     </SubMenu>
-                    <Menu.Item key="6">
-                        <Icon type="pie-chart" />
-                        <span>Option 1</span>
-                    </Menu.Item>
                 </Menu>
                 <div className="rightMain">
                     <Route path="/" exact component={TodoList} />
-                    <Route path="/form" component={Form} />
-                    <Route path="/welcome" component={Welcome} />
-                    <Route path="/video/"   component={Video} />  
-                    <Route path="/workplace/"   component={Workplace} />  
                 </div>
             </div>
         </Router>
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,34 +1,27 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem.js';
 import Storage from '../model/storage.js';
-import { Card, Row, Col, Input, Button, notification, message } from 'antd';
-const footer = {
-    borderTop: '1px solid #ddd'
-}
+import { Card, Row, Col, Input, Button, Tabs,message } from 'antd';
+const { TabPane } = Tabs;
 
-// 点击事件传参
+// onChange事件传参  onChange={event=>{this.changeCheck(event,index)}}
 
 
-const openNotificationWithIcon = type => {
-    notification[type]({
-        message: '添加成功'
-    });
-};
-
+ 
 class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
             todoList: [],
-            todosList:[],
+            todosList: [],
             dothings: "",
             chkStatusIndex: 0
         };
-        this.changeCheck = this.changeCheck.bind(this);
         this.addThings = this.addThings.bind(this);
         this.changeValue = this.changeValue.bind(this);
         this.chkStatus = this.chkStatus.bind(this);
-
+        this.chooseTab = this.chooseTab.bind(this);
+        
 
     }
     componentDidMount() {
@@ -40,29 +33,21 @@ class Home extends Component {
             })
         }
     }
-    changeCheck(e) {
-        console.log(`checked = ${e.target.checked}`);
-        console.log(e);
-    }
- 
+
     // 添加list
-   async addThings() {
+    async addThings() {
 
         if (this.state.dothings) {
-
-            let dataList = [...this.state.todoList,{name:this.state.dothings,checked:false}];
-             // 本地存储
-            Storage.set("todoList",dataList);
-
-          await  this.setState({
-                todoList:dataList,
-                todosList:dataList,
+            let dataList = [...this.state.todoList, { name: this.state.dothings, checked: false }];
+            // 本地存储
+            Storage.set("todoList", dataList);
+            await this.setState({
+                todoList: dataList,
+                todosList: dataList,
                 dothings: ""
             })
-      
-
             this.filterToDoList(this.state.chkStatusIndex);
-            openNotificationWithIcon('success')
+            message.info('添加成功！要及时完成哦，不然要打pp~');
         } else {
             console.log('任务不能为空');
         }
@@ -70,54 +55,38 @@ class Home extends Component {
 
     // 筛选方法
     async  filterToDoList(index) {
- 
+
         if (index === 0) {
             await this.setState({
-                todosList:this.state.todoList
+                todosList: this.state.todoList
             })
         }
-        else if (index === 1) {                                                                                                                                                                                                                                                                                                                                         
-           await this.setState({
-            todosList: this.state.todoList.filter(item => item.checked === false)
+        else if (index === 1) {
+            await this.setState({
+                todosList: this.state.todoList.filter(item => item.checked === false)
             });
         } else if (index === 2) {
             await this.setState({
                 todosList: this.state.todoList.filter(item => item.checked === true)
             });
         }
- 
+
     }
 
     // 删除任务
     delItem(index) {
         let list = this.state.todoList;
-        list.splice(index,1);
-        console.log(list);
+        list.splice(index, 1);
+         
         this.setState({
-            todoList:list
+            todoList: list
         })
-         Storage.set("todoList",list);
+        Storage.set("todoList", list);
+        message.info('删除成功');
         this.filterToDoList(this.state.chkStatusIndex);
-    } 
+    }
 
-       // 编辑时失去焦点
-       todoListBlur(obj) {
-        const { $event, name, index } = obj;
-        if (this.isEdit) {
-          this.isEdit = !this.isEdit;
-        }
-        this.state.todoList.map(item => {
-          if (item.index === index) {
-            item.name = name;
-          }
-          return item;
-        });
-        this.filterToDoList(this.chkStatusIndex);
-      }
- 
-
-
-    changeValue(e){
+    changeValue(e) {
         this.setState({
             dothings: e.target.value
         })
@@ -128,45 +97,47 @@ class Home extends Component {
         this.setState({
             chkStatusIndex: index
         })
- 
+
         this.filterToDoList(index);
     }
+
+    chooseTab(key) {
+        
+        if(key === 0){
+            this.chkStatus(0);
+        }
+        else if(key === 1){
+            this.chkStatus(1);
+        }
+        else if(key === 2){
+            this.chkStatus(2);
+        }
+      }
     render() {
         return (
-            <div className="app">
-                <div>
-                    <Row gutter={15}>
-                        <Col span={15}>
-                            <Input type="text" placeholder="Do what you need to do !" name="dothings"
-                                onChange={this.changeValue} value={this.state.dothings} />
-                        </Col>
-                        <Col span={6}>
-                            <Button type="primary" onClick={this.addThings}>添加</Button>
-                        </Col>
-                    </Row>
-                </div>
-                <div style={{ background: '#ECECEC', padding: '30px', margin: '20px 0' }}>
-
-                    <Card title="todolist" bordered={false} style={{ width: 300 }}>
-                        <TodoItem todoList={this.state.todosList} delItem={this.delItem.bind(this)} todoListBlur={this.todoListBlur.bind(this)}></TodoItem>
-                        {/* {
-                             this.state.todoList.map((item, index) => {
-                                return (
-                                    <div key={index}>
-                                        <Checkbox onChange={this.changeCheck}><span>{item.name}</span></Checkbox>
-                                        <span>{item}</span>
-                                    </div>
-                                )
-                            })
-
-                        } */}
-                        <div className="footer">
-                            <Row>
-                                <Col span={8}>   <Button type="default" onClick={() => { this.chkStatus(0) }}>doing</Button></Col>
-                                <Col span={8}>   <Button type="default" onClick={() => { this.chkStatus(1) }}>actived</Button></Col>
-                                <Col span={8}>   <Button type="default" onClick={() => { this.chkStatus(2) }}>completed</Button></Col>
-                            </Row>
-                        </div>
+            <div className="todo-container">
+                <Row gutter={15} style={{ width: 500 }}>
+                    <Col span={20}>
+                        <Input type="text" placeholder="Do what you need to do !" name="dothings"
+                            onChange={this.changeValue} value={this.state.dothings} />
+                    </Col>
+                    <Col span={4}>
+                        <Button type="primary" onClick={this.addThings}>添加</Button>
+                    </Col>
+                </Row>
+                <div className="mt20">
+                    <Card title="My todoList" bordered={false} style={{ width: 500 }}>
+                        <Tabs defaultActiveKey="1" onChange={this.chooseTab}>
+                            <TabPane tab="Doing" key="1">
+                            <TodoItem todoList={this.state.todosList} delItem={this.delItem.bind(this)} ></TodoItem>
+                            </TabPane>
+                            <TabPane tab="Actived" key="2">
+                            <TodoItem todoList={this.state.todosList} delItem={this.delItem.bind(this)} ></TodoItem>
+                            </TabPane>
+                            <TabPane tab="Completed" key="3">
+                              <TodoItem todoList={this.state.todosList} delItem={this.delItem.bind(this)} ></TodoItem>
+                            </TabPane>
+                        </Tabs>
                     </Card>
                 </div>
             </div>
